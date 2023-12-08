3eb22b8c2a57153cf2321d020dcee445152754aa
Ryan Safarzadeh <safarzadehryan@gmail.com>
2023-12-07 18:14:48 -0800

    finished day 9

---
+++ 9/9a.py
@@ -0,0 +1,31 @@
+visited = set([(0, 0)])
+H = [0, 0]
+T = [0, 0]
+
+with open('input.txt', 'r') as input:
+    for line in input.read().splitlines():
+        x, y = line.split()
+        y = int(y)
+        # print(f'{x} {y}')
+
+        for _ in range(y):
+            dx = 1 if x == "R" else -1 if x == "L" else 0
+            dy = 1 if x == "U" else -1 if x == "D" else 0
+
+            H[0] += dx
+            H[1] += dy
+
+            _x = H[0] - T[0] # x difference 
+            _y = H[1] - T[1] # y difference
+
+            if abs(_x) > 1 or abs(_y) > 1:
+                if _x == 0:
+                    T[1] += _y // 2
+                elif _y == 0:
+                    T[0] += _x // 2
+                else:
+                    T[0] += 1 if _x > 0 else -1
+                    T[1] += 1 if _y > 0 else -1
+
+            visited.add(tuple(T))
+print(len(visited))
