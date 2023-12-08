33d08503c39d67ca9997d6543dc5e9f2847da795
Ryan Safarzadeh <safarzadehryan@gmail.com>
2023-12-08 02:54:26 -0800

    finished day 10

--- 10/10b.py
+++ 10/10b.py
@@ -1,22 +1,32 @@
-crts = []
-with open('input.txt', 'r') as input:
-    crt = []
-    curr_pixel = 1
-    x = 1
-    cycle = 1
-    lines = input.read().splitlines()
-    for line in lines:
-        if line == 'noop':
-            cycle += 1
-            if cycle % 40 == 0:
-                if 
-        else:
-            cycle += 1
-            if cycle % 40 == 0:
-                
-            x += int(line.split()[1])
-            cycle += 1
-            if cycle % 40 == 0:
-                
+from colorama import init, Fore
+init(autoreset=True)
+crt = ''
 
+with open('input.txt', 'r') as input:
+    x = 1
+    cycle = 0
+    ops = input.read().splitlines()
+    for op in ops:
+        cycle += 1
+        if x <= cycle <= x+2:
+            crt += '#'
+        else:
+            crt += '.'
+        if cycle == 40:
+            crt += ' '
+            cycle = 0
+            
+        if op.startswith('addx'):
+            cycle += 1
+            if x <= cycle <= x+2:
+                crt += '#'
+            else:
+                crt += '.'
+            if cycle == 40:
+                crt += ' '
+                cycle = 0
+            x += int(op.split()[1])
     
+    result = crt.split()
+    for row in result:
+        print(row)
