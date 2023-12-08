79d882bdd51ce586e52f90418632e01e0e3e1d3b
Ryan Safarzadeh <safarzadehryan@gmail.com>
2023-12-08 14:28:22 -0800

    finished day 13

---
+++ 13/13a.py
@@ -0,0 +1,24 @@
+with open('input.txt', 'r') as input:
+    x = list(map(str.splitlines, input.read().strip().split('\n\n')))
+    
+    def f(x, y):
+        if type(x) == int:
+            if type(y) == int:
+                return x - y
+            else: 
+                return f([x], y)
+        else:
+            if type(y) == int:
+                return f(x, [y])
+        for a, b in zip(x, y):
+            v = f(a, b)
+            if v:
+                return v
+        return len(x) - len(y)
+
+    t = 0
+    for i, (a, b) in enumerate(x):
+        if f(eval(a), eval(b)) < 0:
+            t += i + 1
+
+    print(t)
---
+++ 13/13b.py
@@ -0,0 +1,27 @@
+with open('input.txt', 'r') as input:
+    x = list(map(eval, input.read().split()))
+    def f(x, y):
+        if type(x) == int:
+            if type(y) == int:
+                return x - y
+            else: 
+                return f([x], y)
+        else:
+            if type(y) == int:
+                return f(x, [y])
+        for a, b in zip(x, y):
+            v = f(a, b)
+            if v:
+                return v
+        return len(x) - len(y)
+    
+    i2, i6 = 1, 2
+
+    for a in x:
+        if f(a, [[2]]) < 0:
+            i2 += 1
+            i6 += 1
+        elif f(a, [[6]]) < 0:
+            i6 += 1
+
+print(i2 * i6)
